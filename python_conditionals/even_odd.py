42eda472e5d1084e43658fc5c28e963517f47b1f
Mohammad Vaqqas <mohammadvaqqas5@gmail.com>
2026-01-28 23:52:06 +0530

    simple even odd program

--- python_conditionals/even_odd.py
+++ python_conditionals/even_odd.py
@@ -1,11 +1,6 @@
-x = float(input("Enter a number : "))
-if (x % 2 == 0) and (x > 0):
-    print("The number is positive even")
-elif (x % 2 != 0) and (x > 0):
-    print("The number is positive odd")
-elif (x % 2 == 0) and (x < 0):
-    print("The number is negative even")
-elif (x % 2 != 0) and (x < 0):
-    print("The number is negative odd")
+x = int(input("Enter a number : "))
+if (x % 2 == 0):
+    print ("Number is even")
 else:
-    print("The number is zero")
+    print ("Number is odd")
+
