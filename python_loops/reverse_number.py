4314fd54b02a190b0d3ef03bf8df5e75cba0bd74
mohammad vaqqas <mohammadvaqqas5@gmail.com>
2026-02-05 08:55:48 +0530

    Added code to reverse a number

---
+++ python_loops/reverse_number.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+reverse = 0
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+print("The reversed number is:", reverse)
