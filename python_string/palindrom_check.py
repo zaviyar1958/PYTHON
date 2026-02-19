ea605dd5ff2938d0d3c1b55c0f0ba267a2372f84
mohammad vaqqas <mohammadvaqqas5@gmail.com>
2026-02-19 13:09:38 +0530

    program to check a string is palindrom or not

---
+++ python_string/palindrom_check.py
@@ -0,0 +1,6 @@
+usr_string = input("Enter your string: ")
+reverse_str = usr_string[-1::-1]
+if usr_string == reverse_str:
+    print("String is palindrom.")
+else : 
+    print("String is not palindrom.")
